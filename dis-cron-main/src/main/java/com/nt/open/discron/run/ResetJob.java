2885595e019aa9bab6565ec8656e4c3f46d0f5bb
coder-fu <fulianqiu123@163.com>
2017-06-23 19:43:26 +0800

    update

--- dis-cron-main/src/main/java/com/nt/open/discron/run/ResetJob.java
+++ dis-cron-main/src/main/java/com/nt/open/discron/run/ResetJob.java
@@ -11,11 +11,20 @@ import com.nt.open.discron.mybatis.ProxyUtil;
  * 检查是有些job因为宕机而任务没有机器执行
  */
 public class ResetJob implements Runnable{
+	
+	private static final long INTERVAL=5*1000L;
 
 	@Override
 	public void run() {
 		JobDao jobDao=(JobDao) ProxyUtil.getProxy(JobDao.class);
 		jobDao.resetJob();
+		
+		try {
+			Thread.sleep(INTERVAL);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
