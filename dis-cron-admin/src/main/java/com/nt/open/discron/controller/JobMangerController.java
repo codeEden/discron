2f2e0282a2facb6227333d0f7dddbdecfef64743
bjfulianqiu <bjfulianqiu@corp.netease.com>
2017-06-26 20:53:01 +0800

    fixed

--- dis-cron-admin/src/main/java/com/nt/open/discron/controller/JobMangerController.java
+++ dis-cron-admin/src/main/java/com/nt/open/discron/controller/JobMangerController.java
@@ -6,6 +6,8 @@ package com.nt.open.discron.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/jobmanager")
 public class JobMangerController {
 
+	Logger logger = LoggerFactory.getLogger(JobMangerController.class);
 	 /**
      * 
      * (跳转到微信消息列表页) 
@@ -29,6 +32,7 @@ public class JobMangerController {
     @RequestMapping(value="/toIndex", method=RequestMethod.GET)
     public String toList(HttpServletRequest req, HttpServletResponse resp)
             throws Exception {
-    	return "/templates/layout/showAll";
+    	logger.info("toIndex  JobManager ");
+    	return "/templates/layout/JobManager";
     }
 }
